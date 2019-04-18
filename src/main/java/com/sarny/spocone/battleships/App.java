d030c445af61f702ce2b72aa985437424ba71754
WojTechM <wojciech.m1997@gmail.com>
2019-04-18 08:01:37 +0200

    Update main method, and pom.xml file so SpringBoot app can be run

--- src/main/java/com/sarny/spocone/battleships/App.java
+++ src/main/java/com/sarny/spocone/battleships/App.java
@@ -1,10 +1,14 @@
 package com.sarny.spocone.battleships;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Kamil Rojek
  */
+@SpringBootApplication
 class App {
     public static void main(String[] args) {
-
+        SpringApplication.run(App.class, args);
     }
 }
