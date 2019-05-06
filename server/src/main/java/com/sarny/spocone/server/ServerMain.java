f031e77def317bc3366d322545e37f7a1d13d553
WojTechM <wojciech.m1997@gmail.com>
2019-05-06 09:47:21 +0200

    Create modules for server and public classes

---
+++ server/src/main/java/com/sarny/spocone/server/ServerMain.java
@@ -0,0 +1,10 @@
+package com.sarny.spocone.server;
+
+/**
+ * @author Kamil Rojek
+ */
+class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+}
