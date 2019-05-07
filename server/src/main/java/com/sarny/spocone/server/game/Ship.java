57528355827f8fc613b8b28de5680e4b0de17b1b
Kamil Rojek <Kamil_Rojek@epam.com>
2019-05-07 10:03:02 +0200

    add ship model

--- server/src/main/java/com/sarny/spocone/server/game/Ship.java
+++ server/src/main/java/com/sarny/spocone/server/game/Ship.java
@@ -1,7 +1,21 @@
 package com.sarny.spocone.server.game;
 
+
+import java.util.*;
+
 /**
  * @author Wojciech Makiela
  */
 class Ship {
+    private List<Integer> toHit;
+    private List<Integer> hit;
+
+    Ship(Integer ... fields) {
+        toHit = List.of(fields);
+        hit = new ArrayList<>();
+    }
+
+    boolean isOnField(int fieldNumber) {
+        return toHit.contains(fieldNumber) || hit.contains(fieldNumber);
+    }
 }
