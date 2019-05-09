b17e2d3c4afd9e1c627b7ec69095445f92350e13
WojTechM <wojciech.m1997@gmail.com>
2019-05-09 14:09:47 +0200

    StubRegister creates valid Game objects

--- server/src/main/java/com/sarny/spocone/server/game/StubRegister.java
+++ server/src/main/java/com/sarny/spocone/server/game/StubRegister.java
@@ -2,6 +2,8 @@ package com.sarny.spocone.server.game;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Wojciech Makiela
@@ -9,13 +11,13 @@ import java.util.Arrays;
 public class StubRegister {
     public Integer player1Id = null;
     public Integer player2Id = null;
-    Board forP1 = new Board(new ArrayList<>(Arrays.asList(
+    private Board forP1 = new Board(new ArrayList<>(Arrays.asList(
             new Ship(new ArrayList<>(Arrays.asList(1, 2, 3, 4))),
-            new Ship(new ArrayList<>(Arrays.asList(20, 21, 22))),
-            new Ship(new ArrayList<>(Arrays.asList(31, 32)))
-            )));
+            new Ship(new ArrayList<>(Arrays.asList(31, 32))),
+            new Ship(new ArrayList<>(Arrays.asList(20, 21, 22)))
+    )));
 
-    Board forP2 = new Board(new ArrayList<>(Arrays.asList(
+    private Board forP2 = new Board(new ArrayList<>(Arrays.asList(
             new Ship(new ArrayList<>(Arrays.asList(1, 10, 20, 30))),
             new Ship(new ArrayList<>(Arrays.asList(3, 13, 23))),
             new Ship(new ArrayList<>(Arrays.asList(6, 16)))
@@ -34,7 +36,10 @@ public class StubRegister {
     }
 
     public Game finalizeCreation() {
-        // TODO pass stub boards to game when Game class is implemented
-        return new Game();
+        Map<Integer, Board> playerBoardMap = new HashMap<>();
+        playerBoardMap.put(player1Id, forP1);
+        playerBoardMap.put(player2Id, forP2);
+        ActiveBoards activeBoards = new ActiveBoards(playerBoardMap);
+        return new Game(activeBoards, player1Id, player2Id);
     }
 }
