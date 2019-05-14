d65d348dd6efb26a086ab5329a1576b8210e516c
WojTechM <wojciech.m1997@gmail.com>
2019-05-14 12:35:47 +0200

    Planned registration on UML diagram - deleted stub register

--- server/src/main/java/com/sarny/spocone/server/gameControllers/ShotController.java
+++ server/src/main/java/com/sarny/spocone/server/gameControllers/ShotController.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.sarny.spocone.publicclasses.shot.Shot;
 import com.sarny.spocone.publicclasses.shot.ShotsSummary;
 import com.sarny.spocone.server.game.Game;
-import com.sarny.spocone.server.game.StubRegister;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,13 +22,11 @@ class ShotController {
     private static Logger logger = LogManager.getLogger(ShotController.class);
     private final Gson gson;
     private final ActiveGames activeGames;
-    private StubRegister register;
 
     @Autowired
     public ShotController(ActiveGames activeGames) {
         this.activeGames = activeGames;
         gson = new Gson();
-        register = new StubRegister();
     }
 
     @PostMapping("/shot")
@@ -91,22 +88,4 @@ class ShotController {
         return guaranteedMissesWithUpdatedOffset;
     }
 
-    // TODO - remove once ShipPlacementController is implemented
-    @PostMapping(path = "/register")
-    public List<Integer> stubRegistration(@RequestBody Integer id) {
-        if (id == null) {
-            return null;
-        }
-        logger.info("Register player with id " + id);
-        List<Integer> fieldsWithShips = register.registerPlayer(id);
-
-
-        if (register.isRegistrationOver()) {
-            logger.info("Registration complete");
-            activeGames.addNewGame(register.finalizeCreation(), register.player1Id, register.player2Id);
-            register = new StubRegister();
-        }
-
-        return fieldsWithShips;
-    }
 }
